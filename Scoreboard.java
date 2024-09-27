e348e372dfbfc585c65b411db71c961d44e79ae9
allllleeeex <agepp27@gmail.com>
2024-09-27 13:30:50 +0000

    hi

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,2 +1,22 @@
-public class Scoreboard{}
+public class Scoreboard {
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
 
+    public Scoreboard(String one, String two) {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+    public void recordPlay(int score)
+    { 
+        teamOneScore = score
+    }
+
+}
